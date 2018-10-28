6f181ab1fc19386d647626d9edcc2b7f813fdd30
Jonah <younes.gh@chmail.ir>
2018-10-28 19:55:20 +0330

    Navbar And SearchBar Added

---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import SearchBar from './searchBar';
+
+const Navbar = () => {
+    return (
+        <nav className="rtl navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+            <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
+                وب سایت شخصی
+            </a>
+
+            <SearchBar />
+
+            <ul className="navbar-nav px-3">
+                <li className="nav-item text-nowrap">
+                    <a className="nav-link" href="#">
+                        خروج
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
